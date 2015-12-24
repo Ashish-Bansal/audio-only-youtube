255ab7cf67d7c9d9cd11a02098ef0474954e0c5c
Ashish Bansal <bansal.ashish096@gmail.com>
2015-12-25 03:17:47 +0530

    Show user that audio only youtube is running

--- audio-only-youtube.js
+++ audio-only-youtube.js
@@ -17,5 +17,19 @@ chrome.runtime.onMessage.addListener(
         var videoElements = document.getElementsByTagName('video');
         var videoElement = videoElements[0];
         videoElement.onloadeddata = makeSetAudioURL(videoElement, url);
+        if (document.getElementsByClassName('audio_only_div').length == 0) {
+            var extensionAlert = document.createElement('div');
+            extensionAlert.className = 'audio_only_div';
+
+            var alertText = document.createElement('p');
+            alertText.className = 'alert_text';
+            alertText.innerHTML = 'Audio Only Youtube Chrome Extension is running. It disables video which saves' +
+                ' bandwidth when you just want to listen to songs. If you want to watch viewo, click on the' +
+                ' extension icon above and refresh your the page.';
+
+            extensionAlert.appendChild(alertText);
+            var parent = videoElement.parentNode.parentNode;
+            parent.appendChild(extensionAlert);
+        }
     }
 );
