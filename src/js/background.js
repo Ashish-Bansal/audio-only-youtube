cdac1e71b99f2f8d66074133bdb3d4a5da56a319
Ashish Bansal <bansal.ashish096@gmail.com>
2018-07-15 19:35:06 +0530

    Run prettier and fix lint errors in whole project

--- src/js/background.js
+++ src/js/background.js
@@ -1,110 +1,115 @@
 class Background {
-    constructor() {
-        this.tabIds = new Map();
+  constructor() {
+    this.tabIds = new Map();
 
-        chrome.storage.local.get('audio_only_youtube_disabled', (values) => {
-            let disabled = values.audio_only_youtube_disabled;
-            if (typeof disabled === "undefined") {
-                disabled = false;
-                this.saveSettings(disabled);
-            }
+    chrome.storage.local.get('audio_only_youtube_disabled', (values) => {
+      let disabled = values.audio_only_youtube_disabled;
+      if (typeof disabled === 'undefined') {
+        disabled = false;
+        this.saveSettings(disabled);
+      }
 
-            if (disabled) {
-                this.disableExtension();
-            } else {
-                this.enableExtension();
-            }
-        });
+      if (disabled) {
+        this.disableExtension();
+      } else {
+        this.enableExtension();
+      }
+    });
 
-        chrome.browserAction.onClicked.addListener(() => {
-            chrome.storage.local.get('audio_only_youtube_disabled', (values) => {
-                let disabled = values.audio_only_youtube_disabled;
+    chrome.browserAction.onClicked.addListener(() => {
+      chrome.storage.local.get('audio_only_youtube_disabled', (values) => {
+        let disabled = values.audio_only_youtube_disabled;
 
-                if (disabled) {
-                    this.enableExtension();
-                } else {
-                    this.disableExtension();
-                }
-
-                disabled = !disabled;
-                this.saveSettings(disabled);
-            });
-
-            chrome.tabs.query({
-                active: true,
-                currentWindow: true,
-                url: "*://www.youtube.com/*"
-            }, (tabs) => {
-                if (tabs.length > 0) {
-                    chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
-                }
-            });
-        });
-    }
-
-    removeURLParameters = (url, parametersToBeRemoved) => {
-        const urlParts = url.split("?");
-        if (urlParts.length < 2) return;
-
-        let currentParameters = urlParts[1].split(/[&;]/g);
-        parametersToBeRemoved.forEach((parameter) => {
-            const prefix = encodeURIComponent(parameter) + '=';
-            currentParameters = currentParameters.filter(p => !p.startsWith(prefix));
-        });
-
-        url = `${urlParts[0]}?${currentParameters.join('&')}`;
-        return url;
-    }
-
-    processRequest = (details) => {
-        const { url, tabId } = details;
-        if (!url.includes("mime=audio")) return;
-        const parametersToBeRemoved = ['range', 'rn', 'rbuf'];
-        const audioURL = this.removeURLParameters(url, parametersToBeRemoved);
-        if (audioURL && this.tabIds.get(tabId) !== audioURL) {
-            this.tabIds.set(tabId, audioURL);
-            chrome.tabs.sendMessage(tabId, { url: audioURL });
+        if (disabled) {
+          this.enableExtension();
+        } else {
+          this.disableExtension();
         }
-    }
 
-    sendMessage = (tabId) => {
-        if (this.tabIds.has(tabId)) {
-            chrome.tabs.sendMessage(tabId, {
-                url: this.tabIds.get(tabId)
-            });
+        disabled = !disabled;
+        this.saveSettings(disabled);
+      });
+
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true,
+          url: '*://www.youtube.com/*',
+        },
+        (tabs) => {
+          if (tabs.length > 0) {
+            chrome.tabs.update(tabs[0].id, { url: tabs[0].url });
+          }
         }
-    }
+      );
+    });
+  }
 
-    enableExtension = () => {
-        chrome.browserAction.setIcon({
-            path: {
-                19: "img/icon19.png",
-                38: "img/icon38.png"
-            }
-        });
-        chrome.tabs.onUpdated.addListener(this.sendMessage);
-        chrome.webRequest.onBeforeRequest.addListener(
-            this.processRequest,
-            {urls: ["<all_urls>"]},
-            ["blocking"]
-        );
-    }
+  removeURLParameters = (url, parametersToBeRemoved) => {
+    const urlParts = url.split('?');
+    if (urlParts.length < 2) return;
 
-    disableExtension = () => {
-        chrome.browserAction.setIcon({
-            path: {
-                19: "img/disabled_icon19.png",
-                38: "img/disabled_icon38.png"
-            }
-        });
-        chrome.tabs.onUpdated.removeListener(this.sendMessage);
-        chrome.webRequest.onBeforeRequest.removeListener(this.processRequest);
-        this.tabIds.clear();
-    }
+    let currentParameters = urlParts[1].split(/[&;]/g);
+    parametersToBeRemoved.forEach((parameter) => {
+      const prefix = encodeURIComponent(parameter) + '=';
+      currentParameters = currentParameters.filter(
+        (p) => !p.startsWith(prefix)
+      );
+    });
 
-    saveSettings = (disabled) => {
-        chrome.storage.local.set({ audio_only_youtube_disabled: disabled });
+    url = `${urlParts[0]}?${currentParameters.join('&')}`;
+    return url;
+  };
+
+  processRequest = (details) => {
+    const { url, tabId } = details;
+    if (!url.includes('mime=audio')) return;
+    const parametersToBeRemoved = ['range', 'rn', 'rbuf'];
+    const audioURL = this.removeURLParameters(url, parametersToBeRemoved);
+    if (audioURL && this.tabIds.get(tabId) !== audioURL) {
+      this.tabIds.set(tabId, audioURL);
+      chrome.tabs.sendMessage(tabId, { url: audioURL });
     }
+  };
+
+  sendMessage = (tabId) => {
+    if (this.tabIds.has(tabId)) {
+      chrome.tabs.sendMessage(tabId, {
+        url: this.tabIds.get(tabId),
+      });
+    }
+  };
+
+  enableExtension = () => {
+    chrome.browserAction.setIcon({
+      path: {
+        19: 'img/icon19.png',
+        38: 'img/icon38.png',
+      },
+    });
+    chrome.tabs.onUpdated.addListener(this.sendMessage);
+    chrome.webRequest.onBeforeRequest.addListener(
+      this.processRequest,
+      { urls: ['<all_urls>'] },
+      ['blocking']
+    );
+  };
+
+  disableExtension = () => {
+    chrome.browserAction.setIcon({
+      path: {
+        19: 'img/disabled_icon19.png',
+        38: 'img/disabled_icon38.png',
+      },
+    });
+    chrome.tabs.onUpdated.removeListener(this.sendMessage);
+    chrome.webRequest.onBeforeRequest.removeListener(this.processRequest);
+    this.tabIds.clear();
+  };
+
+  saveSettings = (disabled) => {
+    chrome.storage.local.set({ audio_only_youtube_disabled: disabled }); // eslint-disable-line
+  };
 }
 
 const background = new Background();
--- src/js/options.js
+++ src/js/options.js
@@ -1,19 +1,24 @@
 // Saves options to chrome.storage
-function save_options() {
+function saveOptions() {
   var showThumbnail = document.getElementById('show-thumbnail');
   chrome.storage.sync.set({
-    showThumbnail: showThumbnail.checked
+    showThumbnail: showThumbnail.checked,
   });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
-  chrome.storage.sync.get({
-    showThumbnail: true
-  }, function(items) {
-    document.getElementById('show-thumbnail').checked = items.showThumbnail;
-  });
+function restoreOptions() {
+  chrome.storage.sync.get(
+    {
+      showThumbnail: true,
+    },
+    function(items) {
+      document.getElementById('show-thumbnail').checked = items.showThumbnail;
+    }
+  );
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('show-thumbnail').addEventListener('change', save_options);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document
+  .getElementById('show-thumbnail')
+  .addEventListener('change', saveOptions);
