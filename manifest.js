caf3241516a654507ac4060165d8699483d33226
Kompwu <37782740+Kompwu@users.noreply.github.com>
2018-08-02 19:31:00 +0200

    refactor: migrate to Parcel

---
+++ manifest.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const path = require('path');
+const root = path.resolve(__dirname);
+
+const dir = {
+  package: path.join(root, 'package.json'),
+  manifest: path.join(root, 'manifest.json'),
+  dist: path.join(root, 'dist'),
+  distManifest: path.join(root, 'dist', 'manifest.json'),
+};
+
+function createManifest(log) {
+  let manifest = JSON.parse(fs.readFileSync(dir.manifest, 'utf8'));
+  let pkg = JSON.parse(fs.readFileSync(dir.package, 'utf8'));
+
+  manifest.name = pkg.displayName;
+  manifest.version = pkg.version;
+  manifest.description = pkg.description;
+
+  fs.writeFileSync(dir.distManifest, JSON.stringify(manifest));
+  if (log) console.info('manifest: created');
+}
+
+function watchOut(something, callback, log) {
+  fs.watchFile(something, () => {
+    if (callback) callback();
+    if (log) console.info(`${something}: updated`);
+  });
+}
+
+if (fs.existsSync(dir.distManifest)) {
+  watchOut(
+    dir.manifest,
+    () => {
+      createManifest();
+    },
+    true
+  );
+  watchOut(
+    dir.package,
+    () => {
+      createManifest();
+    },
+    true
+  );
+} else {
+  if (fs.existsSync(dir.dist)) {
+    createManifest(true);
+    watchOut(
+      dir.manifest,
+      () => {
+        createManifest();
+      },
+      true
+    );
+    watchOut(
+      dir.package,
+      () => {
+        createManifest();
+      },
+      true
+    );
+  }
+}
